7d37ef0b118a7bed60e00311ab6e428ba89f83b7
Wilton Guilherme <wiltoninfra@gmail.com>
2024-10-11 19:02:33 -0300

    Update Mysql and Network

--- azure/mysql/main.tf
+++ azure/mysql/main.tf
@@ -29,15 +29,15 @@ data "azurerm_virtual_network" "this" {
   resource_group_name = var.resource_group_name_vnet
 }
 
-data "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name  # Substitua pelo nome da sua Subnet
+data "azurerm_subnet" "this" {
+  name                 = var.subnet_name # Substitua pelo nome da sua Subnet
   virtual_network_name = var.vnet_name
   resource_group_name  = var.resource_group_name_vnet
 }
 
 # Enables you to manage Private DNS zones within Azure DNS
 resource "azurerm_private_dns_zone" "this" {
-  name                = "${random_string.this.result}.mysql.database.azure.com"
+  name                = "${var.project}-${var.environment}-${random_string.this.result}.mysql.database.azure.com"
   resource_group_name = var.resource_group_name
   tags                = var.tags
 }
@@ -48,7 +48,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   private_dns_zone_name = azurerm_private_dns_zone.this.name
   resource_group_name   = var.resource_group_name
   virtual_network_id    = data.azurerm_virtual_network.this.id
-  tags = var.tags
+  tags                  = var.tags
 }
 
 resource "azurerm_mysql_flexible_server" "this" {
@@ -58,28 +58,61 @@ resource "azurerm_mysql_flexible_server" "this" {
   administrator_login    = var.admin_username
   administrator_password = var.admin_password
   backup_retention_days  = var.backup_retention_days
-  delegated_subnet_id    = data.azurerm_subnet.subnet.id
+  delegated_subnet_id    = data.azurerm_subnet.this.id
   private_dns_zone_id    = azurerm_private_dns_zone.this.id
   sku_name               = var.sku_name
   version                = var.mysql_version
-  
+  zone                   = 1
 
-  high_availability {
-    mode = var.ha_mode
+  dynamic "high_availability" {
+    for_each = var.enable_high_availability ? [1] : []
+    content {
+      mode = var.ha_mode
+    }
   }
 
   storage {
-    size_gb = 20
+    iops    = var.storage_iops
+    size_gb = var.storage_size_gb
+  }
+
+  maintenance_window {
+    day_of_week  = var.maintenance_window_day_of_week
+    start_hour   = var.maintenance_window_start_hour
+    start_minute = var.maintenance_window_start_minute
   }
 
   depends_on = [azurerm_private_dns_zone_virtual_network_link.this]
   tags       = var.tags
 }
 
+#resource "azurerm_mysql_flexible_database" "this" {
+#  name                = "db-${var.project}"
+#  resource_group_name = var.resource_group_name
+#  server_name         = azurerm_mysql_flexible_server.this.name
+#  charset             = "utf8mb4"
+#  collation           = "utf8mb4_unicode_ci"
+#}
+
 resource "azurerm_mysql_flexible_database" "this" {
-  name                = "config-${var.project}-${var.environment}-${random_string.this.result}"
+  for_each           = toset(var.databases)  # Iterar sobre a lista de nomes
+  name               = format("%s-%s", var.db_prefix, var.databases, each.value)  # Concatenar prefixo, projeto e nome
   resource_group_name = var.resource_group_name
-  server_name         = azurerm_mysql_flexible_server.this.name
-  charset             = "utf8mb4"
-  collation           = "utf8mb4_unicode_ci"
+  server_name        = azurerm_mysql_flexible_server.this.name
+  charset            = "utf8mb4"
+  collation          = "utf8mb4_unicode_ci"
+}
+
+resource "azurerm_private_endpoint" "example" {
+  name                = "${random_string.random.result}-endpoint"
+  location            = "Australia East"
+  resource_group_name = "MysqlResourceGroup"
+  subnet_id           = azurerm_subnet.example.id
+
+  private_service_connection {
+    name                           = "${random_string.random.result}-privateserviceconnection"
+    private_connection_resource_id = azurerm_mysql_server.example.id
+    subresource_names              = [ "mysqlServer" ]
+    is_manual_connection           = false
+  }
 }
--- azure/mysql/variables.tf
+++ azure/mysql/variables.tf
@@ -1,70 +1,176 @@
+# Tipo de banco de dados (MySQL ou MySQL Flexível)
+
+# Nome do projeto
 variable "project" {
   description = "Nome do projeto"
   type        = string
 }
 
+# Ambiente (ex: dev, prod, homolog)
 variable "environment" {
-  description = "Ambiente (ex: dev, test, prod)"
+  description = "Ambiente (ex: dev, prod, homolog)"
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "Nome do grupo de recursos"
-  type        = string
-}
-
-variable "location" {
-  description = "Localização para os recursos"
-  type        = string
-}
-
-variable "admin_username" {
-  description = "Nome de usuário do administrador do MySQL"
-  type        = string
-}
-
-variable "backup_retention_days" {
-  description = "Número de dias de retenção de backup"
-  type        = number
-}
-
-variable "subnet_name" {
-  description = "Subnet para o servidor MySQL"
-  type        = string
-}
-
+# Nome do VNet
 variable "vnet_name" {
-  description = "ID da Virtual Network"
+  description = "Nome da Virtual Network"
   type        = string
 }
 
+# Nome da Subnet
+variable "subnet_name" {
+  description = "Nome da Subnet onde o banco será criado"
+  type        = string
+}
+
+# Resource group da VNet
+variable "resource_group_name_vnet" {
+  description = "Nome do Resource Group da VNet"
+  type        = string
+}
+
+# Resource group do banco de dados
+variable "resource_group_name" {
+  description = "Nome do Resource Group onde o banco será criado"
+  type        = string
+}
+
+# Localização (Região) do Azure
+variable "location" {
+  description = "Localização (região) do Azure"
+  type        = string
+}
+
+# Nome de usuário administrador do banco de dados
+variable "admin_username" {
+  description = "Nome de usuário do administrador do banco de dados"
+  type        = string
+}
+
+# Senha do administrador do banco de dados
+variable "admin_password" {
+  description = "Senha do administrador do banco de dados"
+  type        = string
+}
+
+# Nome do banco de dados
+variable "name" {
+  description = "Nome do banco de dados (somente para MySQL tradicional)"
+  type        = string
+  default     = null
+}
+
+# Backup retention (em dias)
+variable "backup_retention_days" {
+  description = "Dias de retenção de backup"
+  type        = number
+  default     = 7
+}
+
+# SKU do banco de dados
 variable "sku_name" {
-  description = "SKU do servidor MySQL"
+  description = "SKU do banco de dados (ex: B_Gen5_1, GP_Gen5_2)"
   type        = string
 }
 
+# Versão do MySQL
 variable "mysql_version" {
-  description = "Versão do MySQL"
+  description = "Versão do MySQL (ex: 5.7, 8.0)"
   type        = string
+  default     = "8.0"
 }
 
+# Tamanho de armazenamento (em GB)
+variable "storage_size_gb" {
+  description = "Tamanho do armazenamento (em GB)"
+  type        = number
+  default     = 20
+}
+
+# IOPS para o armazenamento
+variable "storage_iops" {
+  description = "Número de IOPS para o armazenamento"
+  type        = number
+  default     = 300
+}
+
+# Public network access enabled
+variable "public_network_access_enabled" {
+  description = "Habilitar o acesso de rede pública ao banco de dados (somente para MySQL)"
+  type        = bool
+  default     = false
+}
+
+# Auto grow enabled
+variable "auto_grow_enabled" {
+  description = "Habilitar crescimento automático de armazenamento"
+  type        = bool
+  default     = true
+}
+
+# Geo-redundant backup enabled
+variable "geo_redundant_backup_enabled" {
+  description = "Habilitar backups geo-redundantes"
+  type        = bool
+  default     = false
+}
+
+# Infrastructure encryption enabled
+variable "infrastructure_encryption_enabled" {
+  description = "Habilitar criptografia de infraestrutura"
+  type        = bool
+  default     = false
+}
+
+# High availability enabled
+variable "enable_high_availability" {
+  description = "Habilitar alta disponibilidade para o MySQL Flexível"
+  type        = bool
+  default     = false
+}
+
+# HA mode
 variable "ha_mode" {
-  description = "Modo de alta disponibilidade"
+  description = "Modo de alta disponibilidade (ZoneRedundant, SameZone)"
   type        = string
+  default     = "ZoneRedundant"
 }
 
+# Maintenance window
+variable "maintenance_window_day_of_week" {
+  description = "Dia da semana para janela de manutenção"
+  type        = number
+  default     = 1
+}
+
+variable "maintenance_window_start_hour" {
+  description = "Hora de início da janela de manutenção"
+  type        = number
+  default     = 0
+}
+
+variable "maintenance_window_start_minute" {
+  description = "Minuto de início da janela de manutenção"
+  type        = number
+  default     = 0
+}
+
+# Tags
 variable "tags" {
-  description = "Tags para os recursos"
+  description = "Tags associadas aos recursos"
   type        = map(string)
   default     = {}
 }
 
-variable "admin_password" {
-  description = "Senha do administrador do MySQL"
+variable "db_prefix" {
+  description = "Prefixo do nome do banco de dados"
   type        = string
+  default     = "db"
 }
 
-variable "resource_group_name_vnet" {
-  description = "Nome do grupo de recursos da Virtual Network"
-  type        = string 
+variable "databases" {
+  description = "Lista de nomes dos bancos de dados"
+  type        = list(string)
+  default     = []
 }
